ab28768901840d68753aff7972ef055a692cfb45
WilsomLi <wilsomli@qq.com>
2016-06-19 21:34:05 +0800

    Add Project Files.

---
+++ Assets/Scripts/XEngine/Common/XSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace XEngine
+{
+	public class XSingleton<T> where T : class, new()
+	{
+		protected static T m_instance;
+
+		public static T Instance
+		{
+			get
+			{
+				if(XSingleton<T>.m_instance == null) {
+					XSingleton<T>.m_instance = Activator.CreateInstance<T>();
+				}
+				return XSingleton<T>.m_instance;
+			}
+		}
+	}
+}
+
